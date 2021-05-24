878c4f38d8ea2f4a2a3374968f463c7fe79f577f
Square-Logic <markustryban@squarelogic.digital>
2021-05-24 18:03:04 -0400

    Convert jQuery to JS wip

--- assets/application.js
+++ assets/application.js
@@ -6,3 +6,31 @@ $(document).ready(function() {
     variant_image.show().siblings(':visible').hide();
   })
 });
+
+// function ready(fn) {
+//   if (document.readyState != 'loading'){
+//     fn();
+//   } else {
+//     document.addEventListener('DOMContentLoaded', fn);
+//   }
+// }
+// ready(() => {
+//   let variant_id = this.value;
+//   let variant_image = .single-product-image[data-variant = variant_id]
+
+
+//   document.querySelectorAll(variant_image).forEach(element => {
+//     element.style.display = 'block';
+//     element.siblings(':visible').style.display = 'none';
+//   })
+// });
+
+
+// document.ready(function() {
+//   document.on('change', '#variant-id', function() {
+//     let variant_id = this.value;
+//     let variant_image = $('.single-product-image[data-variant="'+variant_id+'"]');
+
+//     variant_image.style.display = 'block'.siblings(':visible').style.display = "none";
+//   })
+// });
